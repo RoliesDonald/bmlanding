bf16b57ad69938d5f05e0e6daedf444022c99133
rolies donald <rolies.donald@gmail.com>
2025-10-30 14:19:42 +0700

    fix:downgrade React and types to v18 for dewaweb server compatibility

--- next.config.js
+++ next.config.js
@@ -2,22 +2,28 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+// const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+//   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+//   : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/cms', // tambahan 1
+  assetPrefix: '/cms', // tambahan 2
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-        const url = new URL(item)
-
-        return {
-          hostname: url.hostname,
-          protocol: url.protocol.replace(':', ''),
-        }
-      }),
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */]
+        .map((item) => {
+          if (!item) return null // tambahan 3
+          const url = new URL(item)
+          return {
+            hostname: url.hostname,
+            protocol: url.protocol.replace(':', ''),
+          }
+        })
+        .filter(Boolean),
     ],
   },
   webpack: (webpackConfig) => {

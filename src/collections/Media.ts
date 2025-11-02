c163f25534c6e9fb80445ad1e6354fe814c134b4
rolies donald <rolies.donald@gmail.com>
2025-11-02 14:20:23 +0700

    rev media image size

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -48,33 +48,33 @@ export const Media: CollectionConfig = {
         name: 'thumbnail',
         width: 300,
       },
-      {
-        name: 'square',
-        width: 500,
-        height: 500,
-      },
-      {
-        name: 'small',
-        width: 600,
-      },
-      {
-        name: 'medium',
-        width: 900,
-      },
-      {
-        name: 'large',
-        width: 1400,
-      },
-      {
-        name: 'xlarge',
-        width: 1920,
-      },
-      {
-        name: 'og',
-        width: 1200,
-        height: 630,
-        crop: 'center',
-      },
+      // {
+      //   name: 'square',
+      //   width: 500,
+      //   height: 500,
+      // },
+      // {
+      //   name: 'small',
+      //   width: 600,
+      // },
+      // {
+      //   name: 'medium',
+      //   width: 900,
+      // },
+      // {
+      //   name: 'large',
+      //   width: 1400,
+      // },
+      // {
+      //   name: 'xlarge',
+      //   width: 1920,
+      // },
+      // {
+      //   name: 'og',
+      //   width: 1200,
+      //   height: 630,
+      //   crop: 'center',
+      // },
     ],
   },
 }
